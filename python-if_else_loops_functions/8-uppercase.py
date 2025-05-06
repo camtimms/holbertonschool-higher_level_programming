f9c012c61c7a3e8ac9ca7331313fe2c5709c706f
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-05-06 16:06:57 +1000

    8-uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+            if 'a' <= i <= 'z':
+                  upper = chr(ord(i) - 32)
+            else:
+                  upper = i;      
+            print("{}".format(upper),end="")
+    print("")
+    
