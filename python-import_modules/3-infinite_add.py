1f4a782320815dce47aba89c12fe2bd398734c9c
camtimms <cam.g.timms@gmail.com>
2025-05-11 15:07:00 +1000

    pystyle

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 num_args = len(sys.argv) - 1
 total = 0
 
-for i in range(num_args):    
+for i in range(num_args):
     total += int(sys.argv[i + 1])
 
 print(f"{total}")
