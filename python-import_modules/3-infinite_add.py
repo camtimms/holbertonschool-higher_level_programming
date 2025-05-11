6d29f81c162c76ae0c2b256593e9697c8818c1ab
camtimms <cam.g.timms@gmail.com>
2025-05-11 15:05:50 +1000

    3-infinite_add

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+num_args = len(sys.argv) - 1
+total = 0
+
+for i in range(num_args):    
+    total += int(sys.argv[i + 1])
+
+print(f"{total}")
