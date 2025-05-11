86368ef514e40ab41bf95214ece26f0dc667ceb5
camtimms <cam.g.timms@gmail.com>
2025-05-11 15:00:14 +1000

    start from 1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,7 +6,7 @@ num_args = len(sys.argv) - 1
 if num_args > 1:
     print(f"{num_args} arguments:")
     for i in range(num_args):
-        print(f"{i}: {sys.argv[i + 1]}")
+        print(f"{i + 1}: {sys.argv[i + 1]}")
 elif num_args == 1:
     print(f"{num_args} argument:")
     print(f"1: {sys.argv[1]}")
