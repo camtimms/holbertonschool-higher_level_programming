77af543b431f04fb9b934527800bfbfacc4403e3
camtimms <cam.g.timms@gmail.com>
2025-05-11 14:56:40 +1000

    pularity

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,9 +3,12 @@ if __name__ == "__main__":
     import sys
 
 num_args = len(sys.argv) - 1
-if num_args != 0:
+if num_args > 1:
     print(f"{num_args} arguments:")
     for i in range(num_args):
         print(f"{i}: {sys.argv[i + 1]}")
+elif num_args == 1:
+    print(f"{num_args} argument:")
+    print(f"1: {sys.argv[1]}")
 else:
     print(f"{num_args} arguments.")
