142ad4df191279caaed47557bc73804d5e0d1e58
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-05-13 13:45:02 +1000

    refactor

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        if col == len(row):
-            print("{:d}".format(col), end="")
-        else:
             for col in row:
-                print("{:d} ".format(col), end="")
-        print()
+                if col == len(row):
+                    print("{:d}".format(col), end="")
+                else:     
+                    print("{:d} ".format(col), end="")
+            print()
