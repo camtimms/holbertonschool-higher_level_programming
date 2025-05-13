5be39a36bf8bf233eb256aecc7eb021d32ad0783
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-05-13 14:46:09 +1000

    8-multiple_returns

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def new_tuple(tuple_a):
-    if len(tuple_a) == 1:
-        new_tuple = (tuple_a[0], 0)
-    elif len(tuple_a) == 0:
+def new_tuple(tuple):
+    if len(tuple) == 1:
+        new_tuple = (tuple[0], 0)
+    elif len(tuple) == 0:
         new_tuple = (0, 0)
     else:
-        new_tuple = tuple_a
+        new_tuple = tuple
     return (new_tuple)
 
 
@@ -18,3 +18,14 @@ def add_tuple(tuple_a=(), tuple_b=()):
     b = tuple_a[1] + tuple_b[1]
     add_tuple = (a, b)
     return (add_tuple)
+
+'''
+Chatgpt answer
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    a1, a2 = (tuple_a + (0, 0))[:2]
+    b1, b2 = (tuple_b + (0, 0))[:2]
+    return (a1 + b1, a2 + b2)
+
+'''
---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def multiple_returns(sentance):
+    length = len(sentance)
+    
+    if length == 0:
+        first_char = None
+    else:
+        first_char = sentance[0]
+
+    tuple = (length, first_char)
+    return (tuple)
