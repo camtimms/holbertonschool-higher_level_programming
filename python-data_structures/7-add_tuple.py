47a36f7ebf7d8fc98dfec66ab82234f8d0661b2b
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-05-13 14:32:51 +1000

    7-add_tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def new_tuple(tuple_a):
     if len(tuple_a) == 1:
-         new_tuple = (tuple_a[0], 0)
+        new_tuple = (tuple_a[0], 0)
     elif len(tuple_a) == 0:
-         new_tuple = (0, 0)
+        new_tuple = (0, 0)
     else:
-         new_tuple = tuple_a
+        new_tuple = tuple_a
     return (new_tuple)
 
 def add_tuple(tuple_a=(), tuple_b=()):
