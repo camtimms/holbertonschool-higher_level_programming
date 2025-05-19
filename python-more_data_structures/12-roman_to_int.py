2fd2940817f6631a3c03968981bf7cd38b33d62a
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-05-19 15:50:18 +1000

    isinstance

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is not str or None:
-        return (None)
+    if not isinstance(roman_string, str):
+        return (0)
     
     roman_numerals = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
     total = 0
