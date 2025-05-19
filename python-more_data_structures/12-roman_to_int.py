124dd1c1eb8e46da0ed66c7fccd8a6f02927bd08
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-05-19 15:44:33 +1000

    is not

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is str or None:
+    if roman_string is not str or None:
         return (None)
     
     roman_numerals = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
