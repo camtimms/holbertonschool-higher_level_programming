6bb6e5d05aeefcc4c772a12fea65d6e2689657c5
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-05-19 11:28:37 +1000

    0-square_matrix_simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,4 +4,4 @@ def square_matrix_simple(matrix=[]):
     squared_matrix = []
     for row in matrix:
         squared_matrix.append(list(map(lambda x: x**2, row)))
-    return(squared_matrix)
+    return (squared_matrix)
