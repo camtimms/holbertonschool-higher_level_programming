ff0253d6a0248f0db6c9e1e7e367cf13ac2f407e
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-05-19 15:14:32 +1000

    added empty {} check

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary == None:
-        return ("None")
+        return (None)
+    if a_dictionary == {}:
+        return(None)
     # max_value = max(a_dictionary.values())
     
     name = ""
