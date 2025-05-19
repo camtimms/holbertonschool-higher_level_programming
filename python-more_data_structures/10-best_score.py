a0be852674809ab18938c1cd11d63799d610bb7e
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-05-19 15:09:42 +1000

    10-best_score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,5 +2,12 @@
 def best_score(a_dictionary):
     if a_dictionary == None:
         return (None)
-    max_value = max(a_dictionary.values())
-    return (max_value)
+    # max_value = max(a_dictionary.values())
+    
+    name = ""
+    score = 0
+    for key, value in a_dictionary.items():
+        if value > score:
+            score = value
+            name = key
+    return(name)
