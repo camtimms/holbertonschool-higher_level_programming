9ee26e3456574d68b0d1b498be67afec34c3d4a1
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-05-19 16:03:56 +1000

    pystyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,16 +2,22 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return (0)
-    
-    roman_numerals = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+
+    roman_numerals = {"M": 1000, "D": 500, "C": 100,
+                      "L": 50, "X": 10, "V": 5, "I": 1}
     total = 0
     i = 0
 
     while i < len(roman_string):
 
-        # Check if next value is more than the current and substract instead of add
-        if i + 1 < len(roman_string) and roman_numerals[roman_string[i]] < roman_numerals[roman_string[i + 1]]:
-            total += roman_numerals[roman_string[i+1]] - roman_numerals[roman_string[i]] 
+        # Check if next value is more than the current and substract instead of
+        #  add
+        if (i + 1 < len(roman_string) and
+                roman_numerals[roman_string[i]] <
+                roman_numerals[roman_string[i + 1]]):
+
+            total += (roman_numerals[roman_string[i+1]] -
+                      roman_numerals[roman_string[i]])
             i += 1
         else:
             total += roman_numerals[roman_string[i]]
