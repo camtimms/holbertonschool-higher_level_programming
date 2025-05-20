0584c65bea574e9929ce84365366d2d6948d2e86
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-05-20 16:21:44 +1000

    2 dec points

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except:
         return (None)
     finally:
-        print("{}".format(result))
+        print("Inside result: {:.1f}".format(result))
         return (result)
