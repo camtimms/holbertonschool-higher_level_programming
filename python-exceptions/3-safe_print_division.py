dfac8870735fad21a1b38fd075375fe804de0a58
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-05-20 16:26:42 +1000

    corrected for none

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    result = 0
+    result = None
     try:
         result = a / b
     except:
         return (None)
     finally:
-        print("Inside result: {:.1f}".format(result))
-        return (result)
+        if result == None:
+            print("Inside result: None")
+        else:
+            print("Inside result: {:.1f}".format(result))
+    return (result)
