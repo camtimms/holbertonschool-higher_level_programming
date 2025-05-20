f36e26be9c0d188f08431e907a66ced6b514f820
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-05-20 11:44:29 +1000

    0-safe_print_list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    flag = True
     i = 0
-    while flag:
+    while i < x:
         try:
             print(f"{my_list[i]}", end="")
             i += 1
-        except:
-            flag = False
-            print()
+        except IndexError:
+            break
     
+    print()
     return (i)            
