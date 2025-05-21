0c153fb06d5ccbe726e991ff957542c0a432fd60
camtimms <cam.g.timms@gmail.com>
2025-05-21 13:13:12 +1000

    4-list_division.py

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    num = 0
+    den = 0
+    results = []
+    i =0
+    while i < list_length:
+        try:
+            results.append(my_list_1[num] / my_list_2[den]) 
+        except ZeroDivisionError:
+            print("division by 0")
+            results.append(0)
+        except TypeError:
+            print("wrong type")
+            results.append(0)
+        except IndexError:
+            print("out of range")
+            results.append(0)
+        num += 1
+        den += 1
+        i += 1
+    return (results)
