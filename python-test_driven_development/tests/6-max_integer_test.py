3c8ac10dcf6066e2f869a9b4783e7780f3d996ea
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-05-27 16:04:31 +1000

    6-max_integer

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -23,7 +23,12 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([-1, -2, -3]), -1)
         self.assertEqual(max_integer([-10, -5, -20]), -5)
 
+    def test_single_element(self):
+        """Test with a single element"""
+        self.assertEqual(max_integer([5]), 5)
+        self.assertEqual(max_integer([0]), 0)
+        self.assertEqual(max_integer([-5]), -5)
+
     def test_large_numbers(self):
         """Test with large numbers"""
         self.assertEqual(max_integer([1000000, 999999, 1000001]), 1000001)
-        
