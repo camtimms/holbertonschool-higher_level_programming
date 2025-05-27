877fb159c021db747cefeb3a88090bb1b66d724a
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-05-27 13:59:42 +1000

    3-say_my_name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module for say_my_name
+
+Function that prints the users name
+"""
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints the users name
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
