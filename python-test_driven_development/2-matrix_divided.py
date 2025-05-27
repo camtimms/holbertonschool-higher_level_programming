3767eeac48d1763e5808c2f586b61304e5dc781f
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-05-27 13:11:37 +1000

    2-matrix_divided

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+Module for dividing all elements of a matrix
+
+
+'''
+
+def matrix_divided(matrix, div):
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
+    prev_row_len = None
+    for row in matrix:
+        if prev_row_len is None:
+            prev_row_len = len(row)
+        if len(row) != prev_row_len:
+            raise TypeError("Each row of the matrix must have the same size")
+        prev_row_len = len(row)
+    
+    if type(div) not in (int, float):
+        raise TypeError("div must be a number")
+    
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    
+    new_matrix = [list(map(lambda x: round(x/div, 2), row)) for row in matrix]
+
+    return (new_matrix)
