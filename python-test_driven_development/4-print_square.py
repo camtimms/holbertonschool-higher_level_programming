d726e798a112ce8c9b8d6ee37e7d39b394dc0c51
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-05-27 14:27:15 +1000

    4-print_square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module for print_square
+"""
+
+
+def print_square(size):
+    """
+    A function that prints a square with the character #
+    """
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    
+    row = 0
+    col = 0
+    
+    while row < size:
+        col = 0
+        while col < size:
+            print("#", end="")
+            col += 1
+        print("")
+        row += 1
