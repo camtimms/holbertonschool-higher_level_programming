9eae7f791ec1670f84e27bb25e91084089d9e5b4
camtimms <cam.g.timms@gmail.com>
2025-05-28 16:59:00 +1000

    6-square.py

---
+++ python-classes/6-square.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+"""
+Module documentation: Prints a square
+"""
+
+
+class Square:
+    """
+    A class square which holds an atribute size and calculates
+    the area of the square.
+
+    Can now also print the square of the size
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
+        self._position = position
+
+    def area(self):
+        """Calculate the size of a square"""
+        return (self._Square__size**2)
+
+    @property
+    def size(self):
+        """Getter for size"""
+        return (self._Square__size)
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = value
+
+    @property
+    def position(self):
+        return (self._position)
+
+    @position.setter
+    def position(self, tuple):
+        if len(tuple) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        for number in tuple:
+            if number < 0:
+                raise TypeError("position must be a tuple of 2"
+                                " positive integers")
+        self._position = tuple
+
+    def my_print(self):
+        """A function that prints a square with the character #"""
+        if self.size == 0:
+            print("")
+            return
+
+        # Vertical Offset
+        vert_offset = 0
+        while vert_offset < self.position[1]:
+            print("")
+            vert_offset += 1
+
+        row = 0
+        col = 0
+
+        while row < self.size:
+
+            # Horizontal Offset
+            horz_offset = 0
+            while horz_offset < self.position[0]:
+                print(" ", end="")
+                horz_offset += 1
+
+            col = 0
+            while col < self.size:
+                print("#", end="")
+                col += 1
+            print("")
+            row += 1
