33a490c96c54c695b4b52cbcdada0aac7856640e
camtimms <cam.g.timms@gmail.com>
2025-05-28 14:05:23 +1000

    3-square

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module documentation: Adding in a new method to calculate the area of the square
+"""
+
+
+class Square:
+    """
+    A class square which holds an atribute size and calculates the area of the square
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
+
+    def area(self):
+        self.area = self._Square__size**2
+        return(self.area)
