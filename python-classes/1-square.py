355a523c9c31320e8257c46efb554a89fedcc96c
camtimms <cam.g.timms@gmail.com>
2025-05-28 13:53:44 +1000

    1-square

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module documentation: Adding size to the square class
+"""
+
+
+class Square:
+    """
+    A class square which holds an atribute size
+    """
+    def __init__(self, _Square__size):
+        self._Square__size = _Square__size
