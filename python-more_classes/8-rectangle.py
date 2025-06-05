b2973c63c9d136328d6639c6c7d1115b0da64c45
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-06-05 16:43:45 +1000

    8-rectangle.py

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -57,16 +57,16 @@ class Rectangle:
             return (0)
         perimeter = 2*(self.height + self.width)
         return (perimeter)
-    
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle") 
+            raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_2.area() > rect_1.area():
             return (rect_2)
-        return(rect_1)
+        return (rect_1)
 
     def __str__(self):
         """A method that returns a rectangle with the print_symbol character"""
