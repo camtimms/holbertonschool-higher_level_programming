e87002ef6461e28fa8bc745447209defda78c836
camtimms <cam.g.timms@gmail.com>
2025-06-06 15:48:12 +1000

    fixed output for exact match

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,8 +7,8 @@ a specified class
 
 def inherits_from(obj, a_class):
     """ Compares if a object is inherited from a class """
-    #Check for exact matach as it's not a child of a_class
+    # Check for exact matach as it's not a child of a_class
     if type(obj) is a_class:
-        pass
+        return (False)
     else:
         return (issubclass(type(obj), a_class))
