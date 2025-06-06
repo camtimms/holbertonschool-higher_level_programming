425221a0762a479c98f5ddc018976add8d49939a
camtimms <cam.g.timms@gmail.com>
2025-06-06 15:45:41 +1000

    4-inherits_from

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+A function that returns true if the object is inherited from
+a specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """ Compares if a object is inherited from a class """
+    #Check for exact matach as it's not a child of a_class
+    if type(obj) is a_class:
+        pass
+    else:
+        return (issubclass(type(obj), a_class))
