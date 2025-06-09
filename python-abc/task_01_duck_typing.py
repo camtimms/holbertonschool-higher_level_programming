7e5fbcdb6fc7345f7c86074a6556904ab0a59eb0
camtimms <cam.g.timms@gmail.com>
2025-06-09 14:51:44 +1000

    task_01 Duck typing

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+from abc import ABC, abstractmethod
+from math import pi
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self) -> float:
+        pass
+
+    @abstractmethod
+    def perimeter(self) -> float:
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return (pi*self.radius**2)
+
+    def perimeter(self):
+        return (2*pi*self.radius)
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return (self.width * self.height)
+
+    def perimeter(self):
+        return (2*(self.width + self.height))
+
+def shape_info(obj):
+    print(f"Area: {obj.area()}")
+    print(f"Perimter: {obj.perimeter()}")
