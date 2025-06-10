21cc21bcea76fd3010d7b446712853e6186e6959
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-06-10 14:13:40 +1000

    removed import

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,9 +4,10 @@ Name: class_to_json
 Description: Converts an instance of a class to a JSON object
 Returns: JSON Object (dict)
 """
-import json
 
 
 def class_to_json(obj):
     """obj: Instance of a class"""
-    return (json.dumps(obj.__dict__))
+    return (obj.__dict__)
+
+
