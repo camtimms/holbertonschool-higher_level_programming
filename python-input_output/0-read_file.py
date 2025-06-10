236c48745a705088f2818e5776b9b8bcd7883496
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-06-10 10:42:25 +1000

    newline removed

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,7 +5,8 @@ Description: A function that reads a file and prints to stout
 Return: None
 """
 
+
 def read_file(filename=""):
     """filename: Name of file"""
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
