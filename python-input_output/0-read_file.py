31b3dbfb4126e529c77ddc95f35aa16b06c0acc1
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-06-10 10:40:09 +1000

    documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+Name: read_file
+Description: A function that reads a file and prints to stout
+Return: None
+"""
 
 def read_file(filename=""):
+    """filename: Name of file"""
     with open(filename, encoding="utf-8") as f:
         print(f.read())
