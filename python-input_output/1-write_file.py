bac8dc09ad13c6d9998b02f1068f57bf4f2ea420
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-06-10 10:52:30 +1000

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -9,4 +9,4 @@ Return: Number of characters (int)
 def write_file(filename="", text=""):
     """filename: Name of file, text: String to write"""
     with open(filename, 'w', encoding="utf-8") as f:
-        f.write(text)
+        return (f.write(text))
