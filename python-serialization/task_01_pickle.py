43a863051aa3a4c8bfcefae3c3e976600a074b27
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-06-11 17:18:37 +1000

    task 1 pickle

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+Pickle
+
+Description: A task designed to learn about binary serialization using pickle
+"""
+import pickle
+
+class CustomObject:
+    """
+    Basic Object to track student information
+
+    Contains the following atributes:
+    name, age, is_student
+    """
+
+
+    def __init__(self, name, age, is_student) -> None:
+        """Constuctor"""
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def serialize(self, filename):
+        """
+        filename: The filename of the output serialized binary data
+        """
+
+        with open(filename, "wb") as f:
+            data = pickle.dump(self.__dict__, f)
+            return (data)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """
+        cls: Class to be created from data
+        filename: File name to load the data from
+        """
+        with open(filename, "rb") as f:
+            data = pickle.load(f)
+            return(data)
+
+    def display(self):
+        """
+        Display the data of the object
+        """
+        print(
+            f"Name: {self.name}\n"
+            f"Age: {self.age}\n"
+            f"Is Student: {self.is_student}\n"
+        )
