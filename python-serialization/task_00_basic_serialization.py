baf76f32eafe5a06e6e74b28e22f54ace4c80590
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-06-11 16:46:14 +1000

    added type error execption

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -13,9 +13,11 @@ def serialize_and_save_to_file(data, filename):
     data: A python dict with data
     filename: The filename of the output JSON file.
     """
-
-    with open(filename, 'wb') as f:
-        return (pickle.dump(data, f))
+    try:
+        with open(filename, 'wb') as f:
+            return (pickle.dump(data, f))
+    except TypeError:
+        raise TypeError("Error: Non-serializable object")
 
 
 def load_and_deserialize(filename):
@@ -25,6 +27,8 @@ def load_and_deserialize(filename):
     Description: This function returns a Python Dictionary with the deseialized
     JSON data from the file.
     """
-
-    with open(filename, "rb") as f:
-        return (pickle.load(f))
+    try:
+        with open(filename, "rb") as f:
+            return (pickle.load(f))
+    except TypeError:
+        raise TypeError("Error: File is non-serializable")
