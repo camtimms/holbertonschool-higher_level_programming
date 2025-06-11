65856ad2afb20fdc2b3877c8ce7299812be5126a
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-06-11 17:26:59 +1000

    added try, except

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -6,6 +6,7 @@ Description: A task designed to learn about binary serialization using pickle
 """
 import pickle
 
+
 class CustomObject:
     """
     Basic Object to track student information
@@ -13,7 +14,6 @@ class CustomObject:
     Contains the following atributes:
     name, age, is_student
     """
-
 
     def __init__(self, name, age, is_student) -> None:
         """Constuctor"""
@@ -25,10 +25,12 @@ class CustomObject:
         """
         filename: The filename of the output serialized binary data
         """
-
-        with open(filename, "wb") as f:
-            data = pickle.dump(self, f)
-            return (data)
+        try:
+            with open(filename, "wb") as f:
+                data = pickle.dump(self, f)
+                return (data)
+        except TypeError:
+            raise TypeError("Cannot serialize object")
 
     @classmethod
     def deserialize(cls, filename):
@@ -36,11 +38,15 @@ class CustomObject:
         cls: Class to be created from data
         filename: File name to load the data from
         """
-        with open(filename, "rb") as f:
-            obj = pickle.load(f)
-            return(obj)
+        try:
+            with open(filename, "rb") as f:
+                obj = pickle.load(f)
+                return (obj)
+        except TypeError:
+            raise TypeError("Cannot deserialize object")
 
     def display(self):
+
         """
         Display the data of the object
         """
