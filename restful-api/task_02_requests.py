327a604cc6517feb9cba4ea2b28c26b5cf3267cc
camtimms <cam.g.timms@gmail.com>
2025-06-16 11:42:50 +1000

    task 2

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Using the request library to fetch data from an API
+"""
+import requests, csv
+
+def fetch_and_print_posts():
+    r = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print(f"Status Code: {r.status_code}")
+    data = r.json()
+    for post in data:
+        print(post["title"])
+
+def fetch_and_save_posts():
+    r = requests.get('https://jsonplaceholder.typicode.com/posts')
+    data = r.json()
+    with open("posts.csv", 'w') as f:
+        writer = csv.DictWriter(f,fieldnames=data[0].keys())
+        writer.writeheader()
+        writer.writerows(data)
+
