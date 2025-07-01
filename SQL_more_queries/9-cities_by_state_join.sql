7fe42f5cf3675aee292671baf6c52426ff8b1cc0
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-07-01 16:37:18 +1000

    9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 --
 SELECT cities.id, cities.name, states.name
 FROM cities, states
-WHERE cities.state_id = states.state_id
-ORDER BY cities.id
+WHERE cities.state_id = states.id
+ORDER BY cities.id;
