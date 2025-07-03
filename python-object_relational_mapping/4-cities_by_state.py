f5ded553d72143c3c2102c15466c721d1ee1b6f0
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-07-03 16:11:50 +1000

    4-cities_by_state.py

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Script that returns cities form the cities table
+"""
+import MySQLdb
+import sys
+
+
+def get_cities(username, password, dbname):
+    """ Get all cities from cities db table"""
+    try:
+        # Connect to server (Use with for auto closing and management)
+        with MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=username,
+                passwd=password,
+                db=dbname) as db:
+
+            with db.cursor() as cur:
+                cur.execute("SELECT cities.id, cities.name, states.name "
+                            "FROM cities "
+                            "LEFT JOIN states ON states.id = cities.state_id "
+                            "ORDER BY cities.id ASC")
+                # Fetch and display results
+                results = cur.fetchall()
+                for row in results:
+                    print(row)
+
+    except MySQLdb.Error as e:
+        print(f"MySQL Error: {e}")
+
+
+if __name__ == "__main__":
+    get_cities(sys.argv[1], sys.argv[2], sys.argv[3])
