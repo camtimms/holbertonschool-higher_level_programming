008415c665429ba1dcd0caa4239150f183a2bfbd
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-07-03 16:24:45 +1000

    5-filter_cities.py

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Script that returns cities form the cities table
+"""
+import MySQLdb
+import sys
+
+
+def get_cities(username, password, dbname, state_name):
+    """ Get all cities from cities db table"""
+    try:
+        # Connect to server (Use with for auto closing and management)
+        with MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=username,
+                passwd=password,
+                db=dbname) as db:
+
+            with db.cursor() as cur:
+                cur.execute("SELECT cities.name "
+                            "FROM cities "
+                            "LEFT JOIN states ON states.id = cities.state_id "
+                            "WHERE states.name LIKE BINARY %s "
+                            "ORDER BY cities.id ASC", (state_name,))
+                # Fetch and display results
+                results = cur.fetchall()
+                for row in results:
+                    for col in row:
+                        print(col, end="")
+                        if not row == -1:
+                            print(", ", end="")
+                print()
+
+    except MySQLdb.Error as e:
+        print(f"MySQL Error: {e}")
+
+
+if __name__ == "__main__":
+    get_cities(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
