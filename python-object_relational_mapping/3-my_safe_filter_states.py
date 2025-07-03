3e2ddeac41b66027b40ca2f70fcd9624c483891b
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-07-03 15:41:30 +1000

    3-my_safe_filter_states.py

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Script that returns states form the states table
+"""
+import MySQLdb
+import sys
+
+
+def get_states(username, password, dbname, state_name):
+    """ Get all states from states db table"""
+    try:
+        # Connect to server (Use with for auto closing and management)
+        with MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=username,
+                passwd=password,
+                db=dbname) as db:
+
+            with db.cursor() as cur:
+                cur.execute("SELECT * FROM states WHERE name LIKE BINARY "
+                            "'{}' ORDER BY id ASC".format(state_name))
+                # Fetch and display results
+                results = cur.fetchall()
+                for row in results:
+                    print(row)
+
+    except MySQLdb.Error as e:
+        print(f"MySQL Error: {e}")
+
+
+if __name__ == "__main__":
+    get_states(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
