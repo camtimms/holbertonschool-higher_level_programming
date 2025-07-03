42a8dc3aa4db7ba2658839f86fc1779ee9a5c12a
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-07-03 15:11:12 +1000

    1-filter_states.py

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Script that returns states form the states table
+"""
+import MySQLdb
+import sys
+
+
+def get_states(username, password, dbname):
+    """ Get all states from states db table"""
+    try:
+        # Connect to server (Use with for auto closing and management)
+        with MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=username,
+                passwd=password,
+                db=dbname) as db:
+
+            with db.cursor() as cur:
+                cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+                # Fetch and display results
+                results = cur.fetchall()
+                for row in results:
+                    print(row)
+
+    except MySQLdb.Error as e:
+        print(f"MySQL Error: {e}")
+
+
+if __name__ == "__main__":
+    get_states(sys.argv[1], sys.argv[2], sys.argv[3])
