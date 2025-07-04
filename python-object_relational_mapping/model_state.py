44a9a627bd00f7713ca27e322070fc1c8cb68307
camtimms <cam.g.timms@gmail.com>
2025-07-04 16:55:03 +1000

    6-model_state

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+""" State model to be used to create the states table using SQL Alchemy
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True, unique=True)
+    name = Column(String(128), nullable=False)
