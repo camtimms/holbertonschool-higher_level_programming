b685f362ce40f66cd7ba024845021a99ca5e6f0d
camtimms <cam.g.timms@gmail.com>
2025-07-04 16:57:28 +1000

    6-model_state

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """State class used to build a states table using SQL Alchemy"""
     __tablename__ = "states"
