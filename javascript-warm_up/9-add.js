6c4ab130aec769c84754ebce229128fa3fb06a24
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-08-05 14:53:20 +1000

    9-add.js

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const num1 = args[0];
+const num2 = args[1];
+
+function add(a, b) {
+  a = parseInt(a);
+  b = parseInt(b);
+    return(a + b)
+}
+
+console.log(add(num1, num2))
