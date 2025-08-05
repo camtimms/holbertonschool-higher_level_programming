4772ebb5ec3ea9b0aca6e0cd2a7c9a3c6ccf3e8b
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-08-05 15:38:49 +1000

    11-second_biggest.js

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+let largest = 0;
+let prevLargest = 0;
+
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  for (const arg of args) {
+    const num = parseInt(arg);
+
+    if (num > largest) {
+      prevLargest = largest;
+      largest = num;
+    } else if (num > prevLargest && num < largest) {
+      prevLargest = num;
+    }
+  }
+}
+
+console.log(prevLargest);
