6e4cd77591a8b86e5a7f1c8f04ab6f261e9f1c9a
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-08-05 15:09:23 +1000

    10-factorial.js

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const num = parseInt(args[0]);
+
+function factorial (a) {
+  if (a <= 1) {
+    return (1);
+  }
+  else {
+    return (a * factorial(a - 1));
+  }
+}
+
+if (isNaN(num)) {
+  console.log('Missing number');
+} else {
+  console.log(factorial(num));
+}
