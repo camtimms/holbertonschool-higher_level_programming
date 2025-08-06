48cc3e005ecf8c4644b3b98228408f344a532c7e
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-08-06 16:02:51 +1000

    used return instead of raise

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -5,19 +5,24 @@
 def generate_invitations(template, attendees):
     # Check for empty inputs
     if not template:
-        raise ValueError("Template is empty, no output files generated.")
+        print("Template is empty, no output files generated.")
+        return
     if not attendees:
-        raise ValueError("No data provided, no output files generated.")
+        print("No data provided, no output files generated.")
+        return
 
     # Check for input types
     if not isinstance(template, str):
-        raise TypeError("Template is not str, it's a " + str(type(template)))
+        print("Error: Template is not str, it's a " + str(type(template)))
+        return
 
     if not isinstance(attendees, list):
-        raise TypeError("Attendees is not a list, it's a " + str(type(attendees)))
+        print("Error: Attendees is not a list, it's a " + str(type(attendees)))
+        return
 
     if not all(isinstance(attendee, dict) for attendee in attendees):
-        raise TypeError("Not all elements in attendees are dicts")
+        print("Error: Not all elements in attendees are dicts")
+        return
 
     # Use a loop to map the dict values into the string
     for i, attendee in enumerate(attendees, 1):
@@ -31,8 +36,5 @@ def generate_invitations(template, attendees):
 
         # Use edited attendee to format the invitation
         invitation = template.format_map(cleaned_attendee)
-        try:
-            with open(f"output_{i}.txt", "w") as f:
-                f.write(invitation)
-        except FileExistsError:
-            print("Already Exists")
+        with open(f"output_{i}.txt", "w") as f:
+            f.write(invitation)
